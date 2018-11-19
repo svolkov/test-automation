a418c36d730886e9da71296b044fb074681cb623
svolkov <svolkovua@gmail.com>
2018-11-19 22:55:35 +0200

    Preparation for taking screenshots

--- src/test/java/mentoring/automation/SearchGoodsTestCase.java
+++ src/test/java/mentoring/automation/SearchGoodsTestCase.java
@@ -5,13 +5,25 @@ import mentoring.automation.helpers.ConfigPropertiesReader;
 import mentoring.automation.helpers.WebDriverBuilder;
 import mentoring.automation.pages.HomePage;
 import mentoring.automation.pages.SearchResultsPage;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+
 public class SearchGoodsTestCase {
+    private final static String SCREENSHOT_FOLDER = "screenshots";
+    private final static String SCREENSHOT_PATH = WebDriverBuilder.class.getClassLoader()
+            .getResource(SCREENSHOT_FOLDER).getPath();
     private final static String SEARCH_RESULTS_MESSAGE = "Знайдено 260 товарів";
 
     private WebDriver webdriver;
@@ -41,9 +53,23 @@ public class SearchGoodsTestCase {
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void afterMethod(ITestResult results){
         if(webdriver != null){
+            if(results.getStatus() == ITestResult.FAILURE){
+                takeScreenshot(results.getName());
+            }
             webdriver.quit();
+        }
+    }
+
+    private void takeScreenshot(String fileName){
+        TakesScreenshot screenshot = (TakesScreenshot) webdriver;
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        File destination = new File(SCREENSHOT_PATH);
+        try {
+            Files.copy(source.toPath(), destination.toPath(), REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
