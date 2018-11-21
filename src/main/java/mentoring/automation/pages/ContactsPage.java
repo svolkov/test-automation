88dbd6547498f10c6b877c9613e50326846424e4
sergey.volkov <sergey.volkov@worldapp.com>
2018-11-21 13:30:32 +0200

    Adding testCheckSecondKyivPhone

--- src/main/java/mentoring/automation/pages/ContactsPage.java
+++ src/main/java/mentoring/automation/pages/ContactsPage.java
@@ -6,21 +6,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ContactsPage {
     private static final Logger logger = LogManager.getLogger( ContactsPage.class );
-    private static final String PAGE_TITLE_FRAGMENT = "Контактна інформація";
+    private static final String PAGE_TITLE = "Контактна інформація";
     private static final String EXCEPTION_MESSAGE = "Webdriver does not point to the HomePage, current location is ";
     private WebDriver driver;
 
     @FindBy(tagName = "title")
     WebElement contactsPageTitle;
 
+    @FindBy(xpath = "//li[@class='c-i-service-l-i'][2]")
+    WebElement secondKyivPhone;
+
     public ContactsPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
-        if(!contactsPageTitle.getText().equals( PAGE_TITLE_FRAGMENT )){
-            throw new IllegalArgumentException( EXCEPTION_MESSAGE + driver.getCurrentUrl() );
-        }
+        new WebDriverWait( driver, 5).until( ExpectedConditions.titleIs( PAGE_TITLE ) );
+    }
+
+    public String getSecondKyivPhone(){
+        logger.info( "Get second Kyiv phone number" );
+        return secondKyivPhone.getText();
     }
 }
--- src/test/java/mentoring/automation/ContactsPageTestCase.java
+++ src/test/java/mentoring/automation/ContactsPageTestCase.java
@@ -3,10 +3,13 @@ package mentoring.automation;
 import mentoring.automation.constants.SiteLanguage;
 import mentoring.automation.helpers.ConfigPropertiesReader;
 import mentoring.automation.helpers.WebDriverBuilder;
+import mentoring.automation.pages.ContactsPage;
 import mentoring.automation.pages.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class ContactsPageTestCase {
     private WebDriver webdriver;
@@ -20,6 +23,12 @@ public class ContactsPageTestCase {
         if(homePage.getActiveLanguage().equals(SiteLanguage.RU.getName())) {
             homePage.clickSwitchLanguage(SiteLanguage.UA);
         }
+    }
+
+    @Test
+    public void testCheckSecondKyivPhone(){
+        ContactsPage contactsPage = homePage.openContactsPage();
+        Assert.assertEquals(contactsPage.getSecondKyivPhone(), "(044) 503-80-80", "Wrong second Kyiv phone number.");
     }
 
     @AfterMethod
