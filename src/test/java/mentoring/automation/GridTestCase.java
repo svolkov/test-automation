d50278a10c08d967570923fca4ab2df851d5124f
svolkov <svolkovua@gmail.com>
2019-01-07 11:13:00 +0200

    Adding test for file uploads with Grid

--- src/test/java/mentoring/automation/GridTestCase.java
+++ src/test/java/mentoring/automation/GridTestCase.java
@@ -1,7 +1,9 @@
 package mentoring.automation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,14 +18,21 @@ import java.net.URL;
 public class GridTestCase {
     private final static String testUrl = "http://translate.google.com.ua";
     private final static String nodeUrl = "http://192.168.0.104:5566/wd/hub";
+    private final static String testFileUploadUrl = "http://demo.guru99.com/test/upload/";
+    private final static String filePath = "/home/sergey/Pictures/ScreenshotGridConsole.png";
     private WebDriver driver;
+    private By chooseFileButton = By.id("uploadfile_0");
+    private By termsButton = By.id("terms");
+    private By submitButton = By.id("submitbutton");
+    private By resultMessage = By.id("res");
+
 
     @BeforeMethod
-    public void beforeClassMethod(){
-        /*Start Hub: java -jar selenium--standalone-3.141.59.jar -role hub
+    public void beforeMethod(){
+        /*Start Hub: java -jar selenium-server-standalone-3.141.59.jar -role hub
           Start Node: java -Dwebdriver.gecko.driver="/home/sergey/projects/webdrivers/geckodriver" -jar selenium-server-standalone-3.141.59.jar -role webdriver -hub http://192.168.0.104:4444/grid/register -port 5566*/
 
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();//.chrome();
+        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setPlatform(Platform.LINUX);
         capabilities.setBrowserName("firefox");
         capabilities.setVersion("64.0");
@@ -32,7 +41,6 @@ public class GridTestCase {
         }catch (MalformedURLException ex){
             assert false : ex.getMessage();
         }
-
     }
 
     @AfterMethod
@@ -47,5 +55,15 @@ public class GridTestCase {
         driver.get(testUrl);
         new WebDriverWait(driver, 10)
                 .until(ExpectedConditions.titleIs("Google Перекладач"));
+    }
+
+    @Test
+    public void testGridFileUpload(){
+        driver.get(testFileUploadUrl);
+        driver.findElement(chooseFileButton).sendKeys(filePath);
+        driver.findElement(termsButton).click();
+        driver.findElement(submitButton).click();
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.textToBePresentInElementLocated(resultMessage, "has been successfully uploaded."));
     }
 }
